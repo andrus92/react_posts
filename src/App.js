3cc96f58e944946969fef6f79866f2d0e68e1fab
andrus92 <yahoraual92@gmail.com>
2022-10-24 08:48:25 +0200

    Added initial fetch of the data

--- src/App.js
+++ src/App.js
@@ -9,27 +9,16 @@ class App extends React.Component {
 
     this.state = {
       editingPostId: undefined,
-      data : [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-          "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "qui est esse",
-          "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-          "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-        }
-      ]
+      data : []
     }
+  }
+
+  componentDidMount() {
+    // Runs after the first render() lifecycle
+    fetch('https://jsonplaceholder.typicode.com/posts?_start=1&_limit=10')
+      .then((response) => response.json())
+      .then((json) => this.setState({data: json}))
+      .catch(err => console.error(err));
   }
 
   renderPosts = () => {
